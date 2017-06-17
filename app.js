8e45c804e69dceeb736291a2aebf5323f3f82854
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-06-17 19:54:33 -0400

    added some styling

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ $.ajax({
       var logo = data2.follows[i].channel.logo;
       var status = data2.follows[i].channel.status;
       if(logo === null){
-        logo = "https://www.marathonsoftware.com.au/images/intro/anonymouse_224x224.jpg"
+        logo = "http://www.alessandrobozzon.com/img/lab/nopic.png"
       }
       $('#follower-info').prepend("<div class ='row'>" + "<div class='col-md-4'>" + "<img src='" + logo + "'>"
     +
@@ -37,7 +37,25 @@ $.ajax({
   }
 });
 
-
+var deletedFollowers= ['brunofin', 'comster404'];
+for (var i = 0; i < deletedFollowers.length; i++){
+  $.ajax({
+    type: "GET",
+    url: "https://api.twitch.tv/kraken/users/freecodecamp/streams/"+deletedFollowers[i],
+    headers:{
+      'Client-ID': 'l34tkrfqbwfbcwnxlos39z2on9e04h'
+    },
+    error: function(data3){
+      var logo = "http://www.alessandrobozzon.com/img/lab/nopic.png"
+      var displayName = data3.statusText;
+      // console.log(data3.statusText)
+      var status = data3.status;
+      $("#follower-info").prepend("<div class ='row'>" + "<div class='col-md-4'>" + "<img src='" + logo + "'>"
+    +
+  "</div>" + "<div class = 'col-md-4'>" + displayName + "</div> " + "<div class ='col-md-4'>" + status + "</div> </div>")
+    }
+  });
+}
 
 
 
